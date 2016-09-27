52afa1fe01aaa5d013304577ed4a7a76c5fa22b8
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2016-09-28 00:48:15 +0300

    improve algo

--- Cigarette.swift
+++ Cigarette.swift
@@ -8,26 +8,28 @@
 
 import Foundation
 
+
 class Cigarette {
-    
+
     var NowTimestamp = Date().timeIntervalSince1970
     var dateFormatter = DateFormatter()
     var age = 0
     var maxAge = 100
-    var magic = 1.0592193808882908
-    
+    var nicotine = 20.0
+
     func wastedMoney(_ price: Double, _ year: String, _ items: Int)->Double{
         dateFormatter.dateFormat = "yyyy"
         let AgoTimestamp = dateFormatter.date(from: year)?.timeIntervalSince1970
         return ((NowTimestamp - AgoTimestamp!) / (60 * 60 * 24)) * Double(items) * price
     }
-    
-    func realLife()->Double {
-        return Double(maxAge) / magic
+
+    func realLife(_ items: Int)->Double {
+        nicotine *=  Double(items) * 0.5
+        return Double(maxAge) / nicotine
     }
-    
-    func totalLifelost()->Int {
-        return maxAge - Int(self.realLife())
+
+    func totalLifelost(_ items: Int)->Int {
+        return maxAge - Int(self.realLife(1))
     }
     
 }
@@ -36,5 +38,5 @@ let cigarette = Cigarette()
 cigarette.age = 30
 cigarette.maxAge = 90
 cigarette.wastedMoney(4.2, "2015", 1)
-cigarette.realLife()
-cigarette.totalLifelost()
+cigarette.realLife(1)
+cigarette.totalLifelost(1)
